312034ad2a270dab6b70b3b1e042edf4a48fc80a
Mihir Chaturvedi <plibither8@gmail.com>
2022-12-15 13:04:49 +0530

    Allow all headers

--- server/src/main.ts
+++ server/src/main.ts
@@ -25,7 +25,7 @@ const app = express();
 app.use(
   cors({
     origin: "*",
-    methods: ["GET", "POST"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type"],
   })
 );
